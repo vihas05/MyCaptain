c984e9155f6ae3b4f5d83af33df4faf40b9611d5
Vihasini J <66476376+vihas05@users.noreply.github.com>
2020-06-05 19:25:26 +0530

    Add files via upload

---
+++ ltd.py
@@ -0,0 +1,23 @@
+print("Assigning elements to different lists")
+a=["apple",1,6,"mango"]
+b=[]
+c=input("Enter value:")
+b.append(c)
+print (a)
+print (b)
+print("Accessing elements from a tuple")
+t=("apple",5,8,"banana")
+print(t[3])
+print(t)
+print("Deleting different dictionary elements")
+d={"name":"Hardik","number":"33","team":"MI","Nationality":"India"}
+print(d)
+del d["team"]
+print(d)
+
+
+
+
+
+
+
